c0da041600ba02757dc5eb70c4915b7bda7e8b5b
Michael Kline <klinem@gmail.com>
2021-03-18 04:04:50 -0600

    titles

--- client/src/app/RecipeBook.js
+++ client/src/app/RecipeBook.js
@@ -139,14 +139,11 @@ class RecipeBook extends Component {
         </form>
         
         <div>
-                <h3>Todos List</h3>
+                <h3>Recipes</h3>
                 <table className="table table-striped" style={{ marginTop: 20 }} >
                     <thead>
                         <tr>
-                            <th>Description</th>
-                            <th>Responsible</th>
-                            <th>Priority</th>
-                            <th>Action</th>
+                          <th></th>
                         </tr>
                     </thead>
                     <tbody>
