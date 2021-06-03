1c681ed223f9ca24c0f60065a7d33e96d22e2ab6
Michael Kline <klinem@gmail.com>
2021-06-03 13:36:51 -0600

    ready to deploy WIP 0.1

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
 const Port = process.env.PORT;
+const path = require('path');
+require('dotenv').config();
+
 
 app.use(cors());
 app.use(express.urlencoded({
@@ -11,23 +13,34 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 
+
 //conecting to the database, uses db/index.js
 const db = require('./db');
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
+
+//servers React files
+app.use(express.static(path.join('public', 'build')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join('public', 'build', 'index.html'));
+});
+
+
+
+//this use = mounts path for endpoints
 const recipeRouter = require('./routes/recipe_router');
-app.use('/recipe_app', recipeRouter); //use = mounts path
+app.use('/recipe_app', recipeRouter); 
+
+
+
+/* getting from server and displaying /test end point */ 
+app.get('/test', (req, res) => {
+    res.send('Hello World!')
+})
+
 
 
 app.listen(Port, () => {
     console.log(`Server running on port: ${Port}`);
 });
-
-
-
-
-/* getting from server and displaying /test end point */ 
-// app.get('/test', (req, res) => {
-//     res.send('Hello World!')
-// })
